3089a4889b397da6f54a862fbb4f7be7594fe1b2
YangChoMyeong <84573015+YangChoMyeong@users.noreply.github.com>
2021-08-20 16:46:35 +0900

    Update ManageFavoriteWeb.js
    // 문제
    // 1. input에 값을 입력했는데 storage에는 "" 가 들어가는 문제...
    // 2. FORM.addEventListener is not a function 에러가 뜨는데, FORM이 배열이라 작동이 안 되는듯...
    // 3. 계속 새로고침 되는 문제....

--- ManageFavoriteWeb.js
+++ ManageFavoriteWeb.js
@@ -6,24 +6,28 @@
 
 // 문제
 // 1. input에 값을 입력했는데 storage에는 "" 가 들어가는 문제...
+// 2. FORM.addEventListener is not a function 에러가 뜨는데, FORM이 배열이라 작동이 안 되는듯...
+// 3. 계속 새로고침 되는 문제....
 
 
+
+/*
 function main(){
   const A = document.body.getElementsByTagName('a'); // 태그 이름으로 불러온다.
   const FORM = document.body.getElementsByTagName('form');
   const LI = document.body.getElementsByTagName('li');
     for (i = 0; i < A.length; i++) {
-      function aa(idx){
-        FORM[idx].submit = function bb(event){
-          event.preventDefault();
-          function setWeb(idx){
-              const newData= {nickname: NameInput.value, url: UrlInput.value, id:Date.now()};
-              NameInput.value = "";
-              UrlInput.value = "";
-              nList.push(newData); // array에 저장
-              saveData();
-              paintData(newData, idx);
-        }
+          function aa(idx){
+            FORM[idx].submit = function bb(event){
+              event.preventDefault();
+              function setWeb(idx){
+                  const newData= {nickname: NameInput.value, url: UrlInput.value, id:Date.now()};
+                  NameInput.value = "";
+    UrlInput.value = "";
+                  nList.push(newData); // array에 저장
+                  saveData();
+                  paintData(newData, idx);
+            }
 
           function createX(idx){
               const xButton = document.createElement('button');
@@ -47,11 +51,57 @@ function main(){
 
 
 main();
+*/
+
+// 피드백: 잘못 올려주셨거나 IIFE를 잘못 작성한 것 같습니다. 그리고 main 함수가 실행되면 setWeb과 creatX 함수는 선언만 되고 실행은 되지 않을것 같습니다. 선언과 실행은 다르니 참고하시길 바랍니다
+
+const A = document.body.getElementsByTagName('a'); 
+const FORM = document.body.getElementsByTagName('form');
+const LI = document.body.getElementsByTagName('li');
+    
+
+
+function setWeb(idx){
+    const newData= {nickname: NameInput.value, url: UrlInput.value, id:Date.now()};
+    NameInput.value = "";
+    UrlInput.value = "";
+    nList.push(newData); // array에 저장
+    saveData();
+    paintData(newData, idx);
+}
+
+function createX(idx) {
+    const xButton = document.createElement('button');
+    xButton.innerHTML = "❌";
+    LI[idx].appendChild(xButton); 
+    xButton.addEventListener("click", deleteData);   
+}
+
+function paintData(newdata, idx){
+    FORM[idx].classList.add("hidden");
+    A[idx].innerText = newData.nickname;
+    A[idx].href = newData.url;
+    FORM[idx].id = newData.id; 
+    li[idx].onclick = ""; 
+    A[idx].onclick = "";
+} 
+
+// 일단 함수들은 최대한 이렇게 따로 분류
+
+
+function MAIN(){
+    event.preventDefault();
+    for (i = 0; i < A.length; i++) {
+      FORM[idx].submit = createX(i);
+    }
+}
 
 
 
 
-
+  // 일단 function MAIN 안에 function 을 선언해도 MAIN만 실행하면 같이 실행되는 듯. 
+// FORM.addEventListener is not a function 에러가 뜨는데, FORM이 배열이라 작동이 안 되는듯...
+// getElementsByClassName returns an HTMLCollection, not an element. 
 
 
 const firfir = document.body.querySelector(".firfir");
@@ -84,6 +134,7 @@ function deleteData() {
 } // 노마드의 경우, 새로 생성된 객체를 없애면 되는 거지만, 여기는 localStorage에서 없애야....
 // 누른 것에 해당되는 localStorage만 없애기는 어떻게???
 
+//----------------------------------
 /*
 function paintData(newData) {
   first1_Form.classList.add("hidden");
@@ -115,10 +166,25 @@ function setWeb(event) {
   createX();
 }  // 값 저장, 공백, array 저장, 폼 가리고, 결과 나타내고,      x버튼 생성
 
+//first1_Form.addEventListener("submit", setWeb); 설정을 클릭할 때만
+//sButton.addEventListener("click", setWeb);
+
+const getData = localStorage.getItem(NEWLIST); // array에 저장된 각 값 불러오기
+
+if ((getData !== null) &&  (getData !== '[]')) {
+  const parsedList = JSON.parse(getData); // 각 값에 인덱스 부여
+  nList = parsedList; // 인덱스가 부여된 상태 유지 
+  parsedList.forEach(paintData); // 위 과정들을 유지한 상태에서 각 값에 paintData 적용
+  createX();
+} 
 
 
-/* -------------------------------------------
+/// x가 삭제되지 않았던 문제... getData 로 바꾸고나서 단순히 null 이 아니라 '[]'로 해야하는 거였다!!!! 
 
+*/
+
+//-------------------------------------------
+/**
 const A = document.body.getElementsByTagName('a'); // 태그 이름으로 불러온다.
 const FORM = document.body.getElementsByTagName('form');
 const li = document.body.getElementsByTagName('li');
@@ -171,25 +237,12 @@ SET();
     for (i = 0; i < A.length; i++) {
       handlerFn(i);
     }
+
+
 */
 
-//first1_Form.addEventListener("submit", setWeb); 설정을 클릭할 때만
-//sButton.addEventListener("click", setWeb);
 
-const getData = localStorage.getItem(NEWLIST); // array에 저장된 각 값 불러오기
-
-if ((getData !== null) &&  (getData !== '[]')) {
-  const parsedList = JSON.parse(getData); // 각 값에 인덱스 부여
-  nList = parsedList; // 인덱스가 부여된 상태 유지 
-  parsedList.forEach(paintData); // 위 과정들을 유지한 상태에서 각 값에 paintData 적용
-  createX();
-} 
-
-
-/// x가 삭제되지 않았던 문제... getData 로 바꾸고나서 단순히 null 이 아니라 '[]'로 해야하는 거였다!!!! 
-
-
-/*
+/** 
 현재 상황
 
 - 전체로 적용될 수 있으려면,,,,  // 느낌상 function(x) 으로 argument를 미지수로 설정한다음,
@@ -205,13 +258,15 @@ if ((getData !== null) &&  (getData !== '[]')) {
 - ##가 아니라 같은 줄의 여백을 눌러도 toggle이 되는 문제를 어떻게 해결할까? ==>> li 태그 대신 a태그에 토글함수를 부여했더니 되었다!!! li가 display: block; 이었기에 쫙 이어진 것이었다. display: inline; 같은 걸로 했다간 세로가 아니라 가로 배열이 되어버리는 문제..
 li vs a 태그의 크기가 다르다.
 
-/* querySelector와 getElementById, getElemenByClassName 같은거 사용하는 차이 구별!!*/
-/*:first-child 의 경우 div 하위 엘리멘트중에 p 엘리먼트가 가장 첫번째에 위치해야  :first-child 가상클래스를 통해 선택할 수 있기 때문입니다.
+querySelector와 getElementById, getElemenByClassName 같은거 사용하는 차이 구별!!
+:first-child 의 경우 div 하위 엘리멘트중에 p 엘리먼트가 가장 첫번째에 위치해야  :first-child 가상클래스를 통해 선택할 수 있기 때문입니다.
 위 마크업은 div 하위 요소중에 가장 첫번째는 div 엘리먼트이기 때문에 선택할 수 없었던 것입니다. 
-반면에, :firt-of-type 은 실제 p 엘리먼트만을 기준으로 카운트를 하기 때문에 선택할 수 있는 것입니다.*/
+반면에, :firt-of-type 은 실제 p 엘리먼트만을 기준으로 카운트를 하기 때문에 선택할 수 있는 것입니다.
 
-/* 이제 삭제 버튼을 만들고 단체로 적용하면 될듯 싶다!!!*/
-/**##
+이제 삭제 버튼을 만들고 단체로 적용하면 될듯 싶다!!! */
+
+
+/** 
 function createX() {
   const xButton = document.createElement('button');
   xButton.innerHTML = "❌"; //innertext 왜 안 되지.... 그래도 innerHTML 은 되네!! 
@@ -226,9 +281,9 @@ function createX() {
   xButton.addEventListener("click", deleteData); // x버튼을 눌러도 작동하지 않았던 이유가 내가 바깥에다가 xbutton을 만들어놓고 addEventListener 이벤트를 바깥에다가 만들었기에... 어떻게든 setWeb(값을 저장, 화면 출력)과 paint(화면출력 유지) 같은 function에 설정해야한다.
 }
 
-/* first_1.setAttribute("href",`${first1_Url}`); /* 태그의 속성을 바꾸는 방법 */
+ first_1.setAttribute("href",`${first1_Url}`); /* 태그의 속성을 바꾸는 방법 
 
-/**##
+##
 const getNickName = localStorage.getItem("nickname");
 const getUrl = localStorage.getItem("url");
 
@@ -238,10 +293,7 @@ if (getNickName != null && getUrl != null) {
   // 일단 x버튼도 이렇게 두긴 했다....
 }  // 정확히 왜 있는지???? 아, 이게 없으니 계속 유지가 안된다. 이는 값이 없어질 때까지 계속 유지하라는 소리다.
 
-
-*/
-
-/* else{
+ else{
   showMeInput();
   first_1.innerText = "##";
   first_1.href = "";
@@ -249,8 +301,6 @@ if (getNickName != null && getUrl != null) {
   first_1.onclick = "return aa();"; 
 } // 삭제를 진행한 이후로 해야한다. 안 그러면 초기 값과 충돌되는 문제...
 
-
-/** 
 if (getNickName === null || getUrl === null) {
   first1_Form.classList.remove("hidden");
 } else {
@@ -258,5 +308,5 @@ if (getNickName === null || getUrl === null) {
   first_1.innerText = getNickName;
   first_1.href = getUrl;
 } 
-*/
 
+*/
