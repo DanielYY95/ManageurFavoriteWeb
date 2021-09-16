b46c367401f2376475899e4c0e45bbb6ab2b8d61
YangChoMyeong <84573015+YangChoMyeong@users.noreply.github.com>
2021-09-16 14:52:55 +0900

    Update Dropbox.js
    노가다에서 탈피하였음

--- 2nd_branch/JS폴더/Dropbox.js
+++ 2nd_branch/JS폴더/Dropbox.js
@@ -1,41 +1,14 @@
 const Category = document.getElementById("category");
 const Group = ["개인체크", "개발자공부정리", "개발공부", "개발참고", "동기부여", "취미"];
 
-const list_1= document.getElementById("check_list");
-const list_2= document.getElementById("summary_list");
-const list_3= document.getElementById("study_list");
-const list_4= document.getElementById("refer_list");
-const list_5= document.getElementById("motivation_list");
-const list_6= document.getElementById("hobby_list");  
+const List = ["check_list", "summary_list", "study_list", "refer_list", "motivation_list", "hobby_list"]
 
 const X = Category.value;
 
-function selected(X){
- if (X === Group[0]){
-   list_1.appendChild(li);
- }
-
- if (X === Group[1]){
-   list_2.appendChild(li);
- }
-
- if (X === Group[2]){
-   list_3.appendChild(li);
- }
-
-
- if (X === Group[3]){
-   list_4.appendChild(li);
- }
-
-
- if (X === Group[4]){
-   list_5.appendChild(li);
- }
-
- else {
-   list_6.appendChild(li);
- }
- 
-} 
-
+function plsSelect(){
+  if (X === "")
+    {
+      alert ("선택해주세요.");
+      return false;
+    }
+}
