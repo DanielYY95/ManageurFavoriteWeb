e5c083e83e09adddfe2c62e4fd5a96043d425a8f
YangChoMyeong <84573015+YangChoMyeong@users.noreply.github.com>
2021-09-16 14:53:17 +0900

    Update script.js

--- 2nd_branch/JS폴더/script.js
+++ 2nd_branch/JS폴더/script.js
@@ -5,6 +5,7 @@ const LIST=document.body.querySelector("#list");
 const COINLIST="CList";
 const setButton = document.body.querySelector("button:first-of-type");
 
+
 let CList=[]; 
 
 function paintCoin(newCoin){
@@ -18,52 +19,13 @@ function paintCoin(newCoin){
   button.addEventListener("click", deleteCoin);
   li.appendChild(a); 
   li.appendChild(button);
-  
-const Category = document.getElementById("category");
-const Group = ["개인체크", "개발자공부정리", "개발공부", "개발참고", "동기부여", "취미"];
-
-const list_1= document.getElementById("check_list");
-const list_2= document.getElementById("summary_list");
-const list_3= document.getElementById("study_list");
-const list_4= document.getElementById("refer_list");
-const list_5= document.getElementById("motivation_list");
-const list_6= document.getElementById("hobby_list");  
-
-const X = Category.value;
-
-function selected(X){
- if (X === Group[0]){
-   list_1.appendChild(li);
- }
-
- if (X === Group[1]){
-   list_2.appendChild(li);
- }
-
- if (X === Group[2]){
-   list_3.appendChild(li);
- }
-
-
- if (X === Group[3]){
-   list_4.appendChild(li);
- }
-
-
- if (X === Group[4]){
-   list_5.appendChild(li);
- }
-
- else {
-   list_6.appendChild(li);
- }
- 
-} 
-  
-  
-  
-  //selected(Category.value); // 매개변수를 어떻게 해야할지 요게 문제
-  
+  for (i=0; i<6; i++)
+{ if (X === Group[i])
+    {
+      const LIST = document.getElementById(List[i]);
+      LIST.appendChild(li);
+    }
+  }
 }
 
 function saveCoin(){
@@ -72,10 +34,12 @@ function saveCoin(){
 
 function coinInvest(){
   event.preventDefault();
-  if ((inv2.value !=="") && (inv3.value!==""))  { 
-    const newCoin={ Name:inv2.value, Url: inv3.value, id:Date.now()  
+  if ((inv2.value !=="") && (inv3.value!=="") && (Category.value !==""))  { 
+    const newCoin={ Name:inv2.value, Url: inv3.value, Category: Category.value, id:Date.now()  
   };
-    inv2.value=""; inv3.value="";
+    inv2.value=""; 
+    inv3.value="";
+    Category.value="";
     CList.push(newCoin); 
     paintCoin(newCoin);
     saveCoin(); 
