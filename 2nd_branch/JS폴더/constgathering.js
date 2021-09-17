42f7cb84462968f34be661332b28cb43c0536bc5
YangChoMyeong <84573015+YangChoMyeong@users.noreply.github.com>
2021-09-17 15:51:22 +0900

    Update and rename Dropbox.js to constgathering.js

---
+++ 2nd_branch/JS폴더/constgathering.js
@@ -0,0 +1,11 @@
+const nameINPUT= document.body.querySelector("#nameInput");
+const urlINPUT = document.body.querySelector("#urlInput");
+const SITELIST="SList";
+const setButton = document.body.querySelector("button:first-of-type");
+
+let SList=[]; 
+
+const category = document.getElementById("category");
+const Group = ["개인체크", "개발자공부정리", "개발공부", "개발참고", "동기부여", "취미"];
+
+const List = ["check_list", "summary_list", "study_list", "refer_list", "motivation_list", "hobby_list"];
