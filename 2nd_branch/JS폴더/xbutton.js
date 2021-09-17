e3da9b8ce30a114c0418c020001cae145b3bd442
YangChoMyeong <84573015+YangChoMyeong@users.noreply.github.com>
2021-09-17 15:51:56 +0900

    Update xbutton.js

--- 2nd_branch/JS폴더/xbutton.js
+++ 2nd_branch/JS폴더/xbutton.js
@@ -1,14 +1,14 @@
-function deleteCoin(){
+function deleteObj(){
   
   if (!confirm("정말 삭제하시겠습니까?")) {
             alert("취소(아니오)를 누르셨습니다.");
             return; } 
-  else {alert("즐겨찾기가 삭제되었습니다."); 
+  else {
   const li= event.target.parentElement; //target이 대상의 부모
   li.remove();
-  CList=CList.filter((toDo)=>toDo.id !==parseInt(li.id));
-  saveCoin();
-  
+  SList=SList.filter((toDo)=>toDo.id !==parseInt(li.id));
+  saveObj();
+  alert("즐겨찾기가 삭제되었습니다."); 
   }
 
 }
