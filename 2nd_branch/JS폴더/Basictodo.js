8ff6c55ec73bb93b3f8865276c325eb0e4d0c3da
YangChoMyeong <84573015+YangChoMyeong@users.noreply.github.com>
2021-10-08 12:16:18 +0900

    Update Basictodo.js

--- 2nd_branch/JS폴더/Basictodo.js
+++ 2nd_branch/JS폴더/Basictodo.js
@@ -1,23 +1,28 @@
 const LI = document.body.querySelectorAll('div>li');
 const BasicA = document.body.querySelectorAll('li>a');
 const BasicForm = document.body.querySelectorAll("#Basictodo");
+const BasicDiv = document.querySelectorAll("fourthBox>div");
+const SBUTTON = document.querySelectorAll("#setbutton");
 // 공통
 
-const SBUTTON = document.querySelectorAll("#setbutton");
 const NameInput = BasicForm[0].querySelector("input:first-child");
 const UrlInput = BasicForm[0].querySelector("input:nth-child(2)");
 const sButton = BasicForm[0].querySelector("button:first-of-type");
-// FROM의 인덱스에 따라 
-
 const NEWLIST = "nList";
-
 let nList = []; 
+
+// FROM의 인덱스에 따라 
+// 아예 처음부터 nList = [{},{},{}]를 만들까?
+
 
 function saveBasic() {
   localStorage.setItem(NEWLIST, JSON.stringify(nList));
 }
+// 인덱스를 변수로
 
-//1.
+
+// X버튼 누를 때 div를 target으로 가리키게하고, 이것의 하위 태그인 Form과 Li를 불러온다. 허나, x버튼이 Li의 하위태그인디..... 
+//1. Form 인덱스와 LI 인덱스. 현재 Form 과 Li는 div 소속 형제 태그들
 function deleteBasic() {
   const newBasic = { nickname: NameInput.value, url: UrlInput.value, id: Date.now() };
   localStorage.removeItem(NEWLIST);
@@ -28,9 +33,7 @@ function deleteBasic() {
   alert("해당 즐겨찾기를 삭제하였습니다.");
 }
 
-// id를 event.target같은 걸 활용할까?
-
-//2.
+//2. NameInput, UrlInput, nList 인덱스 부여 필요,
 function setBasicTodo() {
   event.preventDefault();
   const newBasic = { nickname: NameInput.value, url: UrlInput.value, id: Date.now() };
@@ -42,7 +45,7 @@ function setBasicTodo() {
   createX();
 }
 
-//3.
+//3. Li인덱스 부여
 function createX() {
   const xButton = document.createElement('button');
   xButton.innerHTML = "❌";
@@ -50,7 +53,7 @@ function createX() {
   xButton.addEventListener("click", deleteBasic);
 } 
 
-//4.
+//4. Form 과 A 태그의 인덱스 부여
 function paintBasic(newBasic) {
   BasicForm[0].classList.add("hidden");
   BasicA[0].innerText = newBasic.nickname;
@@ -58,10 +61,13 @@ function paintBasic(newBasic) {
   BasicForm[0].id = newBasic.id;
 } 
 
+// sButton의 인덱스 부여 필요
 sButton.addEventListener("click", setBasicTodo);
 
+// 불러오는 매개변수의 인덱스 필요
 const getBasic = localStorage.getItem(NEWLIST);
 
+// nList 인덱스 필요
 if ((getBasic !== null) && (getBasic !== '[]')) {
   const parsedList = JSON.parse(getBasic);
   nList = parsedList; 
